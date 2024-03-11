a1d6cff7e0d60d88d7bfaed6daa2e50664d0188d
Omer <ok243@live.mdx.ac.uk>
2024-03-11 13:28:13 +0000

    add home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, HomeComponent],
+  template: '      <header class="brand-name">\n' +
+    '        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />\n' +
+    '      </header>\n' +
+    '      <section class="content">\n' +
+    '        <app-home></app-home>\n' +
+    '      </section>',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'lee';
 }
+
